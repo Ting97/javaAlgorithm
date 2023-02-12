46d1c6015185b8bf6eae275a82127054cf0cf9a8
chenliting <litingchen97@163.com>
2023-02-12 10:58:59 +0800

    [ADD]add 20230204 daily question

---
+++ leetcode/src/main/java/cn/ting97/daily/D20230204.java
@@ -0,0 +1,35 @@
+package cn.ting97.daily;
+
+import java.util.Arrays;
+
+/**
+ * @Classname D20230204
+ * @Description <a href="https://leetcode.cn/problems/maximum-number-of-consecutive-values-you-can-make/">1798.你能构造出连续值的最大数目</a>
+ * @Date 2023/2/12 10:30
+ * @Author by chenlt
+ */
+public class D20230204 {
+    /**
+     * 获取可以从0开始凑出的连续最大数
+     *
+     * @param coins 硬币面额
+     * @return 最大数
+     */
+    public int getMaximumConsecutive(int[] coins) {
+        Arrays.sort(coins);
+        int ans = 1;
+        for (int c : coins) {
+            if (ans >= c) {
+                ans += c;
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int ans = new D20230204().getMaximumConsecutive(new int[]{1, 3});
+        System.out.println("ans = " + ans);
+    }
+}

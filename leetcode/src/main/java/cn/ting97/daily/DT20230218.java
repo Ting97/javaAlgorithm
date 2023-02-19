bead8bbcfe35bb779bbe00a2a2cb6507ce37960a
chenliting <litingchen97@163.com>
2023-02-19 12:43:18 +0800

    [ADD]add 20230218/20230219 daily question

---
+++ leetcode/src/main/java/cn/ting97/daily/DT20230218.java
@@ -0,0 +1,35 @@
+package cn.ting97.daily;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Classname DT20230218
+ * @Description <a href="https://leetcode.cn/problems/find-positive-integer-solution-for-a-given-equation/">1237.找出给定方程的正整数解</a>
+ * @Date 2023/2/19 12:37
+ * @Author by chenlt
+ */
+public class DT20230218 {
+    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int x = 1, y = 1000; x <= 1000 && y >= 1; x++) {
+            while (y >= 1 && customfunction.f(x, y) > z) {
+                y--;
+            }
+            if (y >= 1 && customfunction.f(x, y) == z) {
+                List<Integer> pair = new ArrayList<>();
+                pair.add(x);
+                pair.add(y);
+                res.add(pair);
+            }
+        }
+        return res;
+    }
+
+    public interface CustomFunction {
+        // Returns f(x, y) for any given positive integers x and y.
+        // Note that f(x, y) is increasing with respect to both x and y.
+        // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+        int f(int x, int y);
+    }
+}
---
+++ leetcode/src/main/java/cn/ting97/daily/DT20230219.java
@@ -0,0 +1,43 @@
+package cn.ting97.daily;
+
+import java.util.PriorityQueue;
+
+/**
+ * @Classname DT20230219
+ * @Description <a href="https://leetcode.cn/problems/maximum-average-pass-ratio/">1792.最大平均通过率</a>
+ * @Date 2023/2/19 12:37
+ * @Author by chenlt
+ */
+public class DT20230219 {
+
+    public double maxAverageRatio(int[][] classes, int extraStudents) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            long val1 = (long) (b[1] + 1) * b[1] * (a[1] - a[0]);
+            long val2 = (long) (a[1] + 1) * a[1] * (b[1] - b[0]);
+            if (val1 == val2) {
+                return 0;
+            }
+            return val1 < val2 ? 1 : -1;
+        });
+        for (int[] c : classes) {
+            pq.offer(new int[]{c[0], c[1]});
+        }
+
+        for (int i = 0; i < extraStudents; i++) {
+            int[] arr = pq.poll();
+            assert arr != null;
+            int pass = arr[0], total = arr[1];
+            pq.offer(new int[]{pass + 1, total + 1});
+        }
+
+        double res = 0;
+        for (int i = 0; i < classes.length; i++) {
+            int[] arr = pq.poll();
+            assert arr != null;
+            int pass = arr[0], total = arr[1];
+            res += 1.0 * pass / total;
+        }
+        return res / classes.length;
+    }
+
+}

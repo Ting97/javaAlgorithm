4e0a1803955fdeee87ee732710d00f1351cbcb8b
chenliting <litingchen97@163.com>
2023-02-23 08:43:04 +0800

    [ADD]add 20230223 daily question

---
+++ leetcode/src/main/java/cn/ting97/daily/DT20230223.java
@@ -0,0 +1,21 @@
+package cn.ting97.daily;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Classname DT20230223
+ * @Description <a href="https://leetcode.cn/problems/circular-permutation-in-binary-representation/">1238.循环码排列</a>
+ * @Date 2023/2/23 08:38
+ * @Author by chenlt
+ */
+public class DT20230223 {
+
+    public List<Integer> circularPermutation(int n, int start) {
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < 1 << n; i++) {
+            ret.add((i >> 1) ^ i ^ start);
+        }
+        return ret;
+    }
+}

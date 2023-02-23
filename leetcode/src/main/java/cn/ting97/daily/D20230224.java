73c45b7af6037958e3a38601a4b7c5505056c709
chenliting <litingchen97@163.com>
2023-02-24 00:48:07 +0800

    [ADD]add 20230224 daily question

---
+++ leetcode/src/main/java/cn/ting97/daily/D20230224.java
@@ -0,0 +1,16 @@
+package cn.ting97.daily;
+
+import java.util.Arrays;
+
+/**
+ * @Classname D20230224
+ * @Description <a href="https://leetcode.cn/problems/make-array-zero-by-subtracting-equal-amounts/">2357.使数组中所有元素都等于零</a>
+ * @Date 2023/2/24 00:44
+ * @Author by chenlt
+ */
+public class D20230224 {
+
+    public int minimumOperations(int[] nums) {
+        return (int) Arrays.stream(nums).filter(i -> i != 0).distinct().count();
+    }
+}
